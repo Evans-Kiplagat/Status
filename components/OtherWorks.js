61d25b030c661b75006c1516f2443c5039671783
Evans kiplagat <evansrotichh7@gmail.com>
2023-12-19 11:18:52 +0300

    changes

--- components/OtherWorks.js
+++ components/OtherWorks.js
@@ -12,91 +12,93 @@ const OtherWorks = () => {
         <div className={styles.projectContainer} data-aos='zoom-in'>
           <img src='/assets/stack.svg' className={styles.stackIcon} />
           <a
-            href='https://github.com/noqdev/iambic'
+            href='https://github.com/liquiidio/EosWsSharp'
             target='_blank'
             rel='noopener noreferrer'
           >
             <FiExternalLink className={styles.linkIcon} />
           </a>
-          <p className={styles.projectName}>IAMbic Open Source</p>
+          <p className={styles.projectName}>EosWsSharp</p>
           <p className={styles.aboutProject}>
-            IAMbic tackles the complex task of managing distributed permissions
-            across multiple cloud environments, such as AWS, Azure Active
-            Directory, Okta, and Google Workspace.
+             C# dfuse.io Websocket-Client.
+             Working with the different dfuse EOSIO APIs and the open-source dfuse for EOSIO binary.
           </p>
           <div className={styles.projectToolsWrapper}>
-            <p className={styles.toolName}>Python</p>
-            <p className={styles.toolName}>IAM</p>
-            <p className={styles.toolName}>GCP</p>
-            <p className={styles.toolName}>AWS</p>
+            <p className={styles.toolName}>C#</p>
+            <p className={styles.toolName}>EOS</p>
+            <p className={styles.toolName}>Websockets</p>
+            <p className={styles.toolName}>Websocket-client</p>
+            <p className={styles.toolName}>Blockchain</p>
+            <p className={styles.toolName}>dfuse</p>
           </div>
         </div>
         <div className={styles.projectContainer} data-aos='zoom-in'>
           <img src='/assets/stack.svg' className={styles.stackIcon} />
           <a
-            href='https://github.com/eniolastyle/Matchapp-Server-CI-CD'
+            href='https://github.com/liquiidio/EosioSigningRequestSharp'
             target='_blank'
             rel='noopener noreferrer'
           >
             <FiExternalLink className={styles.linkIcon} />
           </a>
-          <p className={styles.projectName}>Matchapp-Server CI/CD</p>
+          <p className={styles.projectName}>EOSIO Request Signing Sharp</p>
           <p className={styles.aboutProject}>
-            A mini backend AI project focused on one endpoint that takes in
-            multiple resumes and a job description returning the best matches.
+          The ESR protocol allows for an application (dapp) to generate signature requests (transactions)
+          which can then be passed to signers (wallets) for signature creation. These signature requests can be used within URI links,
+          QR Codes, or other transports between applications and signers.
           </p>
           <div className={styles.projectToolsWrapper}>
-            <p className={styles.toolName}>Python</p>
+            <p className={styles.toolName}>EOSIO</p>
             <p className={styles.toolName}>Docker</p>
-            <p className={styles.toolName}>HCL</p>
-            <p className={styles.toolName}>GHA</p>
-            <p className={styles.toolName}>AWS</p>
+            <p className={styles.toolName}>Cloud wallets</p>
+            <p className={styles.toolName}>.NET</p>
+            <p className={styles.toolName}>C#</p>
           </div>
           {/* https://check-d-deck.herokuapp.com */}
         </div>
         <div className={styles.projectContainer} data-aos='zoom-in'>
           <img src='/assets/stack.svg' className={styles.stackIcon} />
           <a
-            href='https://github.com/eniolastyle/asg-rolling-update-cicd'
+            href='https://github.com/liquiidio/CloudWalletUnity'
             target='_blank'
             rel='noopener noreferrer'
           >
             <FiExternalLink className={styles.linkIcon} />
           </a>
-          <p className={styles.projectName}>ASG Rolling Update</p>
+          <p className={styles.projectName}>Cloud Wallet Plugin</p>
           <p className={styles.aboutProject}>
-            The project provides an example of a rolling update deployment
-            strategy for an Auto Scaling Group.
+          A native integration of the Wax Cloud Wallet compatible with 
+          all major Build Targets (WebGL, Windows, Mac, Linux, Android, iOS) without relying on WebViews.
           </p>
           <div className={styles.projectToolsWrapper}>
-            <p className={styles.toolName}>HCL</p>
-            <p className={styles.toolName}>Python</p>
-            <p className={styles.toolName}>Shell</p>
-            <p className={styles.toolName}>AWS</p>
+            <p className={styles.toolName}>.Net</p>
+            <p className={styles.toolName}>Unity</p>
+            <p className={styles.toolName}>Unity Package Manager</p>
+            <p className={styles.toolName}>WebGL</p>
           </div>
         </div>
         <div className={styles.projectContainer} data-aos='zoom-in'>
           <img src='/assets/stack.svg' className={styles.stackIcon} />
           <a
-            href='https://github.com/eniolastyle/zenith-microservice-ecs-terraform-checkov'
+            href='https://github.com/liquiidio/AnchorLinkSharp'
             target='_blank'
             rel='noopener noreferrer'
           >
             <FiExternalLink className={styles.linkIcon} />
           </a>
           {/* <a href="https://allcountriesdetails.netlify.app/" target="_blank" rel="noopener noreferrer"><FiExternalLink className={styles.linkIcon} /></a> */}
-          <p className={styles.projectName}>ECS Microservice Deployment</p>
+          <p className={styles.projectName}>Anchor Link Sharp</p>
           <p className={styles.aboutProject}>
-            The goal of the repository is to provide an example implementation
-            of deploying a microservice architecture on AWS ECS using Terraform,
-            while also incorporating infrastructure security scanning with
-            Checkov.
+          A native integration compatible with Unity3D and C# allowing users and developers
+          to connect and communicate with Anchor Wallet and ESR-based applications.
+          The Anchor & ESR Integration consists of multiple libraries for the ESR-Protocol, the Anchor-integration,
+          Transports etc. which will be included via Submodules while being packaged and published as a single Package.
           </p>
           <div className={styles.projectToolsWrapper}>
-            <p className={styles.toolName}>VueJs</p>
-            <p className={styles.toolName}>HCL</p>
-            <p className={styles.toolName}>AWS</p>
-            <p className={styles.toolName}>Docker</p>
+            <p className={styles.toolName}>.net</p>
+            <p className={styles.toolName}>Unity</p>
+            <p className={styles.toolName}>C#</p>
+            <p className={styles.toolName}>Anchor wallet</p>
           </div>
         </div>
         <div className={styles.projectContainer} data-aos='zoom-in'>
@@ -121,108 +123,100 @@ const OtherWorks = () => {
         <div className={styles.projectContainer} data-aos='zoom-in'>
           <img src='/assets/stack.svg' className={styles.stackIcon} />
           <a
-            href='https://github.com/eniolastyle/asg-lb-module'
+            href='https://github.com/liquiidio/UniversalAuthenticatorLibraryUnity'
             target='_blank'
             rel='noopener noreferrer'
           >
             <FiExternalLink className={styles.linkIcon} />
           </a>
-          <p className={styles.projectName}>AWS ASG-ALB Module</p>
+          <p className={styles.projectName}>Universal Authenticator Library Sharp (UAL)</p>
           <p className={styles.aboutProject}>
-            The repository serves as a reusable and configurable module for
-            setting up an ASG with an associated LB in AWS using Terraform. It
-            simplifies the process of creating scalable and load-balanced
-            infrastructure in the AWS cloud.
+          A native UAL allowing the use of supported SignatureProviders, 
+          similar to the js-based UAL allowing developers and users the same interaction flow and UI/UX on all different platforms.
+           The priority of this plugin is for user and developer experience,
+            while building the same base with extension-capabilities allowing to support 
+            additional SignatureProviders like Wombat, MetaMask, AIKON, other Wallets or SideChain-Auth in the future.
           </p>
           <div className={styles.projectToolsWrapper}>
-            <p className={styles.toolName}>HCL</p>
-            <p className={styles.toolName}>Shell</p>
+            <p className={styles.toolName}>Unity</p>
+            <p className={styles.toolName}>.NET</p>
           </div>
         </div>
         <div className={styles.projectContainer} data-aos='zoom-in'>
           <img src='/assets/stack.svg' className={styles.stackIcon} />
           <a
-            href='https://github.com/eniolastyle/voting-app-k8s'
+            href='https://github.com/liquiidio/AtomicMarketApiClient'
             target='_blank'
             rel='noopener noreferrer'
           >
             <FiExternalLink className={styles.linkIcon} />
           </a>
-          <p className={styles.projectName}>Voting App</p>
+          <p className={styles.projectName}>AtomicMarketApiClient</p>
           <p className={styles.aboutProject}>
-            The repository serves as a reference or starting point for deploying
-            and managing containerized applications using Kubernetes. It
-            showcases how to leverage Kubernetes features to create a scalable
-            and resilient voting application.
-          </p>
+            The repository serves as a reference .NET and Unity3D-compatible (Desktop, Mobile, WebGL) ApiClient for AtomicMarket.
+            </p>
           <div className={styles.projectToolsWrapper}>
-            <p className={styles.toolName}>K8s</p>
-            <p className={styles.toolName}>YAML</p>
-            <p className={styles.toolName}>Docker</p>
+            <p className={styles.toolName}>.net</p>
+            <p className={styles.toolName}>web3</p>
+            <p className={styles.toolName}>WebGL</p>
           </div>
         </div>
 
         <div className={styles.projectContainer} data-aos='zoom-in'>
           <img src='/assets/stack.svg' className={styles.stackIcon} />
           <a
-            href='https://devopsnode.hashnode.dev'
+            href='https://github.com/liquiidio/HyperionApiClient'
             target='_blank'
             rel='noopener noreferrer'
           >
             <FiExternalLink className={styles.linkIcon} />
           </a>
-          <p className={styles.projectName}>ASG Article</p>
+          <p className={styles.projectName}>HyperionApiClient</p>
           <p className={styles.aboutProject}>
-            In this tutorial, I explore how to provision an AWS Load balancer
-            and Auto Scaling Group (ASG) using Terraform and deploy it using
-            GitHub Actions (GHA).
+          .NET and Unity3D-compatible (Desktop, Mobile, WebGL) Client-Library for Hyperion History APIs
           </p>
           <div className={styles.projectToolsWrapper}>
-            <p className={styles.toolName}>HCL</p>
-            <p className={styles.toolName}>Terraform</p>
-            <p className={styles.toolName}>AWS</p>
-            <p className={styles.toolName}>GHA</p>
+            <p className={styles.toolName}>.net</p>
+            <p className={styles.toolName}>web3</p>
+            <p className={styles.toolName}>WebGL</p>
           </div>
         </div>
         <div className={styles.projectContainer} data-aos='zoom-in'>
           <img src='/assets/stack.svg' className={styles.stackIcon} />
           <a
-            href='https://devopsnode.hashnode.dev'
+            href='https://github.com/liquiidio/WombatUnityWebGl'
             target='_blank'
             rel='noopener noreferrer'
           >
             <FiExternalLink className={styles.linkIcon} />
           </a>
-          <p className={styles.projectName}>EC2 & LB Article</p>
+          <p className={styles.projectName}>WombatUnityWebGl</p>
           <p className={styles.aboutProject}>
-            In this tutorial, I demonstragted how to create an AWS Load Balancer
-            with Terraform, and how distribute the server load in a 50-50 manner
-            with load balancer.
+          .NET and Unity3D-compatible (Desktop, Mobile, WebGL) ApiClient for the different APIs. 
+          Endpoints have its own set of parameters that you may build up and pass in to the relevant function.
           </p>
           <div className={styles.projectToolsWrapper}>
-            <p className={styles.toolName}>HCL</p>
-            <p className={styles.toolName}>Terraform</p>
-            <p className={styles.toolName}>AWS</p>
+          <p className={styles.toolName}>.net</p>
+            <p className={styles.toolName}>web3</p>
+            <p className={styles.toolName}>WebGL</p>
           </div>
         </div>
         <div className={styles.projectContainer} data-aos='zoom-in'>
           <img src='/assets/stack.svg' className={styles.stackIcon} />
           <a
-            href='https://devopsnode.hashnode.dev'
+            href='https://github.com/liquiidio/AtomicAssetsApiClient'
             target='_blank'
             rel='noopener noreferrer'
           >
             <FiExternalLink className={styles.linkIcon} />
           </a>
-          <p className={styles.projectName}>Agile Article</p>
+          <p className={styles.projectName}>  ApiClient for AtomicAssets</p>
           <p className={styles.aboutProject}>
-            In this article, I provided an overview of Agile methodology and its
-            core principles, emphasizing its focus on iterative product delivery
-            and customer collaboration.
+          .NET and Unity3D-compatible (Desktop, Mobile, WebGL) ApiClient for AtomicAssets
           </p>
           <div className={styles.projectToolsWrapper}>
-            <p className={styles.toolName}>Agile</p>
-            <p className={styles.toolName}>SCRUM</p>
+            <p className={styles.toolName}>.NET</p>
+            <p className={styles.toolName}>WebGL</p>
           </div>
         </div>
       </div>
